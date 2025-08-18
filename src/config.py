54cbca04d4f0db8d3ffba93dfa9bd110854fd5c0
sillock <mail@sillock.com>
2025-08-18 19:16:13 +0100

    fix: config file and merged settings

---
+++ src/config.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, PydanticBaseSettingsSource, SettingsConfigDict, YamlConfigSettingsSource
+
+class MotSettings(BaseSettings):
+  mot_client_id: str
+  mot_tenant_id: str
+  mot_client_secret: str
+  mot_api_key: str
+
+class ScraperSettings(BaseModel):
+    chrome_binary_location: str
+    enable_sandbox: bool
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(yaml_file="config.yaml")
+
+    enabled_sites: list[str]
+    scraper: ScraperSettings
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (YamlConfigSettingsSource(settings_cls), )
+
+settings = Settings() # type: ignore
+mot_settings = MotSettings() # type: ignore
--- src/settings.py
+++
@@ -1,8 +0,0 @@
-from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    client_id: str
-    tenant_id: str
-    client_secret: str
-    api_key: str
