76f00c80e39ff93954e421e4493470805ebd84d5
sillock <mail@sillock.com>
2025-08-21 23:26:29 +0100

    fix: injected ves api not used

--- src/vehicle_service.py
+++ src/vehicle_service.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from mot_api_service import MotErrorResponse, MotSummaryInformation
 from ves_api_service import VehicleInformation, VehicleErrorResponse
-from generated.ves_client import Client as VesClient
 import mot_api_service
 import ves_api_service
 
@@ -10,9 +9,7 @@ class VehicleAndMotInformation(BaseModel):
     vehicle_information: VehicleInformation | VehicleErrorResponse | None
 
 async def fetch_vehicle_information(registration_plate: str) -> VehicleAndMotInformation:
-  headers = {'accept': 'application/json'}
-  ves_client = VesClient(base_url="https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry", headers=headers)
-  vehicle_information = await ves_api_service.fetch_vehicle_info(ves_client, registration_plate)
+  vehicle_information = await ves_api_service.fetch_vehicle_info(registration_plate)
   mot_summary = await mot_api_service.fetch_mot_history(registration_plate)
   vehicle_and_mot_information = VehicleAndMotInformation.model_validate({"mot_information" : mot_summary, "vehicle_information": vehicle_information})
   return vehicle_and_mot_information
