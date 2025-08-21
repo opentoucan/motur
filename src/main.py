bd877443c4ee12f66d7f9d508363551b563e163f
sillock <mail@sillock.com>
2025-08-21 23:44:24 +0100

    chore: move class up

--- src/main.py
+++ src/main.py
@@ -6,15 +6,15 @@ from fastapi.exceptions import HTTPException
 from urllib.parse import urlparse
 from config import settings
 
+class WebpageScrape(BaseModel):
+   url: str
+
 app = FastAPI()
 
 @app.get("/reg/{reg}")
 async def read_mot_from_reg(reg: str):
     vehicle_information = await vehicle_service.fetch_vehicle_information(reg)
     return vehicle_information.model_dump()
-
-class WebpageScrape(BaseModel):
-   url: str
 
 @app.post("/link")
 async def read_mot_from_webpage(webpage: WebpageScrape):
